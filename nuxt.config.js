c94203215a636bac43d8fe347b98de68f12ca107
Loïc Gamache <lgamache@nmedia.ca>
2021-06-08 15:33:23 -0400

    add config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
+  router: {
+    base: '/nms-vue-components/',
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'nms-vue-components-nuxt',
